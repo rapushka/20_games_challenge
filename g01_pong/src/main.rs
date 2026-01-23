2cbecc3bfe330da1af3fba92761fddc99cf6ed1f
rapuska <mikalajsiatkouski@gmail.com>
2026-01-23 23:00:07 +0300

    spawn camera

--- g01_pong/src/main.rs
+++ g01_pong/src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run()
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2d);
 }
