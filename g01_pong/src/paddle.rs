e21fa4dc55c093ebb715ad617085e7092f19014b
rapuska <mikalajsiatkouski@gmail.com>
2026-01-24 00:24:21 +0300

    refactor a bit

--- g01_pong/src/paddle.rs
+++ g01_pong/src/paddle.rs
@@ -1,5 +1,7 @@
-use bevy::window::PrimaryWindow;
 use crate::prelude::*;
+
+pub use create::*;
+mod create;
 
 const PADDLE_SIZE: Vec2 = Vec2::new(25.0, 125.0);
 
@@ -10,33 +12,4 @@ pub struct Paddle;
 pub enum Side {
     Left,
     Right,
-}
-
-pub fn spawn_paddles(
-    mut commands: Commands,
-    windows: Query<&Window, With<PrimaryWindow>>,
-) {
-    let window = windows.single().unwrap();
-    let half_width = window.resolution.width() * 0.5 - 100.0;
-
-    create_paddle(&mut commands, -half_width, Side::Left);
-    create_paddle(&mut commands, half_width, Side::Right);
-}
-
-fn create_paddle(
-    commands: &mut Commands,
-    x_position: f32,
-    side: Side,
-) {
-    commands.spawn((
-        Paddle,
-        side,
-        Collider(PADDLE_SIZE),
-        Sprite {
-            color: Color::srgb(0.9, 0.9, 0.9),
-            custom_size: Some(PADDLE_SIZE),
-            ..default()
-        },
-        Transform::from_xyz(x_position, 0.0, z_order::PADDLE),
-    ));
 }
