b867c66c36c4e0b2b46915c9596b6d691ba7bb72
rapuska <mikalajsiatkouski@gmail.com>
2026-01-29 23:15:29 +0300

    spawn camera

--- g02_jetpack_joyride/src/main.rs
+++ g02_jetpack_joyride/src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_camera)
+        .run()
+}
+
+fn spawn_camera(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2d);
 }
