34329ccb4389475de0f1a43ec1559950ca8ca43b
rapuska <mikalajsiatkouski@gmail.com>
2026-02-02 14:54:03 +0300

    refactor color util

--- g02_jetpack_joyride/src/utils.rs
+++ g02_jetpack_joyride/src/utils.rs
@@ -1,5 +1,12 @@
 use crate::prelude::*;
 
-pub fn color_from(hex: &'static str) -> Color {
-    Srgba::hex(hex).unwrap().into()
+const ERROR_PINK: Color = Color::srgb_u8(255, 0, 255);
+
+pub fn from_hex(hex: &'static str) -> Color {
+    if let Ok(color) = Srgba::hex(hex) {
+        return color.into();
+    }
+
+    error!("Can't parse Color from Hex: {}", hex);
+    ERROR_PINK
 }
